b7433b10eb73065ad630d979a49938150bca450a
Tom Wilson <twilson63@gmail.com>
2021-07-07 10:13:14 -0400

    3-reactivity

--- src/views/Current.svelte
+++ src/views/Current.svelte
@@ -1,5 +1,13 @@
 <script>
+  import { fahrenheitToCelsius, celsiusToFahrenheit } from 'temperature'
+
   import Weather from '../components/Weather.svelte'
+
+    let temp = 21.7;
+    let unit = "c"
+
+    $: displayTemp = `${Math.floor(temp)} &deg; ${unit.toUpperCase()}`
+    
 
   const weather = {
     city: 'Charleston, SC',
@@ -7,17 +15,27 @@
     icon: 'a01n',
       description: 'clear',
       foo: 'bar'
+    
+  }
+  function convertToF() {
+    temp = celsiusToFahrenheit(temp)
+    unit = "f"
+  }
+  function convertToC() {
+    temp = fahrenheitToCelsius(temp)
+    unit = "c"
   }
 </script>
 <nav>
   <div>
-    <a href="">F</a>
+    <a href="" on:click|preventDefault={convertToF}>F</a>
     |
-    <a href="">C</a>
+    <a href="" on:click|preventDefault={convertToC}>C</a>
   </div>
   <a href="">Favorites</a>
 </nav>
 <main>
+  {@html displayTemp}
   <Weather {...weather} />
 </main>
 <style>
